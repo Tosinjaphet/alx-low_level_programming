3b52217ba9b75ab163cd9a5ae5c5e518f714a3f5
Ashogbon Oluwatosin <Tosinjaphet@gmail.com>
2023-03-16 08:05:54 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,10 @@
 #include "main.h"
 #include <stdio.h>
-/*_puts_recursion without using printf*/
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
