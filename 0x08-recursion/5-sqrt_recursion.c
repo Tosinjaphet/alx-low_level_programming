001f3734a5c84b4944f3329d386fe3b7a2993d05
Ashogbon Oluwatosin <Tosinjaphet@gmail.com>
2023-03-27 09:55:42 -0700

    wellit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,22 +12,22 @@ int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt(0, n));
+	return (sqrt(0, n));
 }
 
 /**
- * _sqrt - returns the square root of a number
+ * sqrt - returns the square root of a number
  * @n: test number
  * @x: squared number
  *
  * Return: the square root of n
  */
 
-int _sqrt(int n, int x)
+int sqrt(int n, int x)
 {
 	if (n > x / 2)
 		return (-1);
 	else if (n * n == x)
 		return (n);
-	return (_sqrt(n + 1, x));
+	return (sqrt(n + 1, x));
 }
