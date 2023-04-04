36a331d48b81e9d6253a91319d83e94f469e21ed
Ashogbon Oluwatosin <tosinjaphet@gmail.com>
2023-04-04 16:06:47 -0700

    corrected singly list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,19 @@
 #include "lists.h"
-
 /**
- * print_listint - prints all the elements of a linked list
- * @h: begining of the list
- *
- * Return: the number of nodes
+ * print_listint - print integer
+ * @h: the element containing the list
+ * Return: the node of the linked list
  */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+	size_t node_num = 1;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
-}
-return (count);
+	while (h->next != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		node_num++;
+	}
+	printf("%d\n", h->n);
+	return (node_num);
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,27 +1,21 @@
 #include "lists.h"
-
 /**
-*free_listint2 - frees a linked list
-*@head: pointer to the head of the list
-*
-*Return: void
-*/
-
+ * free_listint2 - free linked list
+ * @head: free head of the list
+ * Return: nothing
+ */
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
+	listint_t *current, *temp;
 
-if (temp != NULL)
-{
-while (*head != NULL)
-{
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
-}
-
-*temp = NULL;
-
-}
+	if (head != NULL)
+	{
+		current = *head;
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
 }
